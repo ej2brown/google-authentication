2baf049ead734df274f8daddc97b737441db1f35
Elizabeth Brown <elizabeth.brown@pinchfinancial.com>
2022-03-09 13:06:21 -0500

    Bundled

--- build/bundle.js
+++ build/bundle.js
@@ -1,21 +1,10 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 $(document).on(() => {
   console.log("ready!");
+  
+  gapi.load('auth2', initialize);
 
-  const authInstance = gapi.auth2.getAuthInstance({
-    client_id: proess.env.GOOGLE_CLIENT_ID || ""
-  }).then(() => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', proess.env.SERVER_URL);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onload = function () {
-      console.log('Signed in as: ' + xhr.responseText);
-    };
-    xhr.send('idtoken=' + id_token);
-  });
-
-
-  gapi.load('auth2', () => {
+  const initialize = () => {
     console.log('Initializing...');
 
     gapi.auth2
@@ -37,8 +26,19 @@ $(document).on(() => {
 
     // Start with the current values.
     renderUserInfo(auth2.currentUser.get().getBasicProfile());
-  });
+  };
 
+  const authInstance = gapi.auth2.getAuthInstance({
+    client_id: proess.env.GOOGLE_CLIENT_ID || ""
+  }).then(() => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', proess.env.SERVER_URL);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function () {
+      console.log('Signed in as: ' + xhr.responseText);
+    };
+    xhr.send('idtoken=' + id_token);
+  });
 
   /**
    * Listener method for sign-out live value.
